d080b1191ddcc1267d484c3f5d0c40f3a3f104e1
TrapikKolz <striffemen@gmail.com>
2022-06-21 17:14:06 +0300

    Определили модель пользователя

--- src/notebook/accounts/models.py
+++ src/notebook/accounts/models.py
@@ -1,3 +1,81 @@
-from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.db.models import EmailField, CharField, BooleanField, DateTimeField
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(
+            self, email, password=None, name=None, full_name=None, is_active=True, is_staff=None, is_admin=None
+    ):
+        if not email:
+            raise ValueError("Пользователь должен иметь email")
+        if not password:
+            raise ValueError("Пользователь должен иметь пароль")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, full_name=full_name)
+        user.password = make_password(password)
+        user.staff = is_staff
+        user.admin = is_admin
+        user.is_active = is_active
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email=None, password=None, name=None):
+        user = self.create_user(email, name=name, password=password, is_staff=True, is_admin=True)
+        return user
+
+    def create_staffuser(self, email=None, password=None,  name=None):
+        user = self.create_user(email, name=name, password=password, is_staff=True, is_admin=False)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = EmailField(unique=True, max_length=255)
+    first_name = CharField(max_length=255, blank=True, null=True)
+    second_name = CharField(max_length=255, blank=True, null=True)
+    staff = BooleanField(default=False)
+    is_active = BooleanField(default=True)
+    admin = BooleanField(default=False)
+    timestamp = DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_short_name(self):
+        if self.first_name:
+            return self.first_name
+        return self.email
+
+    def get_short_full_name(self):
+        if self.first_name and self.second_name:
+            self.full_name = self.second_name + self.first_name
+            return self.full_name
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        if self.admin:
+            return True
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    def save(self, *args, **kwargs):
+        if not self.id and not self.staff and not self.admin:
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
