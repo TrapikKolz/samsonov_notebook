4cb331ef4f11179dbe63d15f57e749cfaa41523e
TrapikKolz <striffemen@gmail.com>
2022-07-07 17:52:23 +0800

    Создали шаблон, определили путь, создали вьюху

---
+++ src/notebook/notes/utils.py
@@ -0,0 +1,5 @@
+class Mymixin(object):
+    mixin_prop = ''
+
+    def mixin_func(self):
+        return self.mixin_prop.upper()
--- src/notebook/notes/views.py
+++ src/notebook/notes/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 
-# Create your views here.
+from .utils import Mymixin
+from .models import Note
+
+
+class HomeView(Mymixin, ListView):
+    model = Note
+    template_name = 'notes/index.html'
+    context_object_name = 'notes'
+    mixin_prop = ''
+    #paginate_by = 2
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        context['mixin_prop'] = self.mixin_func()
+        return context
+
+    def get_queryset(self):
+        return Note.objects.order_by('-updated').select_related('category')
